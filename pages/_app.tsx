21fa65cfa9586885e1a828e5702d44b27ba798a9
Griffen Fargo <ghfargo@gmail.com>
2023-07-25 00:44:00 -0400

    Update _app.tsx and package.json
    - Modified _app.tsx in the pages directory to include the Analytics component from @vercel/analytics/react.
    - Modified package.json to add the "@vercel/analytics" dependency.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,20 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { AppProvider } from '@/components/AppContext';
 import { Inter } from 'next/font/google';
-import { Toaster } from '@/components/ui/toaster';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 function App({ Component, pageProps }: AppProps<{}>) {
   return (
-    <AppProvider>
-      <main className={inter.className}>
-        <Component {...pageProps} />
-      </main>
-    </AppProvider>
+    <>
+      <AppProvider>
+        <main className={inter.className}>
+          <Component {...pageProps} />
+        </main>
+      </AppProvider>
+      <Analytics />
+    </>
   );
 }
 
